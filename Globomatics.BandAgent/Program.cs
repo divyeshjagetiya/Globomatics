4af83cdf436085c5333e9733635ae073cb8398b4
Divyesh Jagetiya <djagetiya@XE-T-DIVYESHJAG>
2018-07-04 16:07:01 +0530

    Add project files.

---
+++ Globomatics.BandAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Globomatics.BandAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
