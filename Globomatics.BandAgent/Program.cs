ee976be0517648bf8d7f2a7bf6dc2b2a51cf87e1
Divyesh Jagetiya <djagetiya@XE-T-DIVYESHJAG>
2018-07-06 11:54:13 +0530

    Project Initiate : First Commit

--- Globomatics.BandAgent/Program.cs
+++ Globomatics.BandAgent/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿/* 
+ * Divyesh Jagetiya
+ * Date: 05-July-2018
+ * Code followed from https://app.pluralsight.com/player?course=azure-iot-hub-developers-getting-started&author=matt-honeycutt&name=a7db97d2-7d1e-40d1-8acd-a889e1f17087&clip=1&mode=live
+ */
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
 
 namespace Globomatics.BandAgent
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void  Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             Console.WriteLine("Hello..");
         }
     }
 }
---
+++ Globomatics.BandAgent/readMe.txt
@@ -0,0 +1,11 @@
+﻿
+1) Create Console application in which install Microsoft.Device.Client nuget package.
+
+2) Download IoTHub Explorer from "https://github.com/Azure/iothub-explorer/releases" or Write in Terminal "npm install -g iothub-explorer"
+iothub-explorer help
+iothub-explorer login "HostName=<my-hub>.azure-devices.net;SharedAccessKeyName=<my-policy>;SharedAccessKey=<my-policy-key>"
+iothub-explorer get known-device --connection-string
+iothub-explorer create new-device --connection-string
+iothub-explorer delete existing-device
+
+3) 
