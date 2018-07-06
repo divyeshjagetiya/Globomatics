8eae9247e6018a0b80e80bf7a281d24895444301
Divyesh Jagetiya <djagetiya@XE-T-DIVYESHJAG>
2018-07-06 14:03:50 +0530

    Sending Message to IoT hub and Getting data using iothub-explorer monitor-events

--- Globomatics.BandAgent/Program.cs
+++ Globomatics.BandAgent/Program.cs
@@ -1,10 +1,10 @@
 ﻿/* 
  * Divyesh Jagetiya
  * Date: 05-July-2018
- * Code followed from https://app.pluralsight.com/player?course=azure-iot-hub-developers-getting-started&author=matt-honeycutt&name=a7db97d2-7d1e-40d1-8acd-a889e1f17087&clip=1&mode=live
  */
 using System;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices.Client;
 
@@ -25,12 +25,29 @@ namespace Globomatics.BandAgent
             //We have created our client now it's time to connect with the hub. So OpenAsync will help us to connect with HUB.
             await device.OpenAsync();
             Console.WriteLine("Device is Connected!");
+            //Our Device is connected now let's put our data into the cloud 
+            // Obviously we will not write while(true) in production side, this will be just testing purpose
+            while (true)
+            {
+                //For Sending a message to cloud we have to Create instance of Message class which is provided by IoT hub.
+                //Message need payload(i.e input) but that payload shoud be in Array of bytes not a string. So we have to convert string into Array of bytes.
+                //So lets first convert string into Array of bytes and then we will give that varibale as input of Message.
+                var stringToArrayBytes = Encoding.ASCII.GetBytes("Hello from Band Agent");
+                var message = new Message(stringToArrayBytes);
+                
+                //Its time to send a message to cloud.
+                await device.SendEventAsync(message);
+                Console.WriteLine("Message send to the cloud");
 
+                //lets take break for 2 second after sending a message.
+                Thread.Sleep(2000);
+                //We can now send a simple message from our Device to Hub. 
+                //Open Ternimal (lets say CMD1) : write => iothub -explorer monitor-events --login "Azure IoT Hub Connection String"
+                //Now run the Globomatics.BandAgent Solution, We can see that from Device we are sending messages and in CMD1 we are receiving.
+
+            }
             Console.WriteLine("Press Key to exit..");
             Console.ReadKey();
-
-
-
         }
     }
 }
--- README.md
+++ README.md
@@ -1,25 +1,27 @@
 
 1) Create Console application in which install Microsoft.Device.Client nuget package.
+<br/> And also create one device as "device-01" in Azure IoT Hub. Get ConnectionString from portal.
 #
 2) We have already created our project locally so now its time to do first commit in Github.
-   Use below code in your terminal to push our code.
-a) git init 
-b) git status
-c) git add .
-d) git commit -m "first commit"
-e) git remote add origin "github repository link"
+   Use below code in your terminal to push our code.<br/>
+a) git init <br/>
+b) git status<br/>
+c) git add .<br/>
+d) git commit -m "first commit"<br/>
+e) git remote add origin "github repository link"<br/>
 f) git push -u origin master
 #
 3) Now lets create Development branch so that we will commit our all changes to Development first and then will merge to Master branch
-a) git checkout -b "Development"
+<br/>a) git checkout -b "Development"<br/>
 b) git push -u origin Development
 #
 4) Download IoTHub Explorer from "https://github.com/Azure/iothub-explorer/releases" or Write in Terminal "npm install -g iothub-explorer"
-<br/><br/>iothub-explorer help
+<br/>This will help us to connect our Azure Iot hub from Terminal(cmd).<br/> 
+<br/>iothub-explorer help
 <br/>iothub-explorer login "HostName='my-hub'.azure-devices.net;SharedAccessKeyName='my-policy';SharedAccessKey='my-policy-key'"
 <br/>iothub-explorer login "HostName='my-hub'.azure-devices.net;SharedAccessKeyName='my-policy';SharedAccessKey='my-policy-key'"
 <br/>iothub-explorer get known-device --connection-string
 <br/>iothub-explorer create new-device --connection-string
 <br/>iothub-explorer delete existing-device
-
+#
 5) 
